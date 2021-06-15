00eaf084244829b9e9bc27cc5e1a83fbb2440703
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-06-15 09:41:39 -0400

    Get input from inputEl push to myLeads array

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-// get input id and store it in a variable
-let inputButton = document.getElementById('input-btn');
+let myLeads = [];
+// get input-el id and store it in a variable
+const inputEl = document.getElementById('input-el');
+// get input-btn id and store it in a variable
+const inputButton = document.getElementById('input-btn');
+
 // Add Event Listener when clicked invoke function
 inputButton.addEventListener('click', function () {
-	console.log('Clicked!');
+	// push input value to myLead variable
+	myLeads.push(inputEl.value);
+	console.log(myLeads);
 });

2397a8f950f8ec218ada4cf41f74421705342019
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-06-15 09:17:58 -0400

    Add evernt listener

---
+++ index.js
@@ -0,0 +1,6 @@
+// get input id and store it in a variable
+let inputButton = document.getElementById('input-btn');
+// Add Event Listener when clicked invoke function
+inputButton.addEventListener('click', function () {
+	console.log('Clicked!');
+});

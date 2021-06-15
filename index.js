0f151dce6d3687611ced91c7964fef6275fae65e
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-06-15 16:45:40 -0400

    Create Render Function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ['www.awesomelead.com', 'www.epiclead.com', 'www.greatlead.com'];
+let myLeads = [];
 // get input-el id and store it in a variable
 const inputEl = document.getElementById('input-el');
 // get input-btn id and store it in a variable
@@ -10,14 +10,16 @@ const ulEl = document.getElementById('ul-el');
 inputButton.addEventListener('click', function () {
 	// push input value to myLead variable
 	myLeads.push(inputEl.value);
-	console.log(myLeads);
+	renderLeads();
 });
-// Assign listItems to empty string
-let listItems = '';
-for (let i = 0; i < myLeads.length; i++) {
-	// Add item to the listItems variable
-	listItems += '<li>' + myLeads[i] + '</li>';
-	console.log(listItems);
+
+function renderLeads() {
+	// Assign listItems to empty string
+	let listItems = '';
+	for (let i = 0; i < myLeads.length; i++) {
+		// Add item to the listItems variable
+		listItems += '<li>' + myLeads[i] + '</li>';
+	}
+	// Render the listItems inside the unordered list
+	ulEl.innerHTML = listItems;
 }
-// Render the listItems inside the unordered list
-ulEl.innerHTML = listItems;

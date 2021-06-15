47db6f7806eacaa8cddbb57a24ff5135f458f409
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-06-15 16:28:42 -0400

    Test innerHTML

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let myLeads = [];
+let myLeads = ['www.awesomelead.com', 'www.epiclead.com', 'www.greatlead.com'];
 // get input-el id and store it in a variable
 const inputEl = document.getElementById('input-el');
 // get input-btn id and store it in a variable
 const inputButton = document.getElementById('input-btn');
+// Get unordered list store it in a variable
+const ulEl = document.getElementById('ul-el');
 
 // Add Event Listener when clicked invoke function
 inputButton.addEventListener('click', function () {
@@ -10,3 +12,7 @@ inputButton.addEventListener('click', function () {
 	myLeads.push(inputEl.value);
 	console.log(myLeads);
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+	ulEl.innerHTML += '<li>' + myLeads[i] + '</li>';
+}
